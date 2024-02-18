89bc1c4bcc20af644f6118e31594cd113fdee984
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2024-02-18 04:36:27 +0000

    Added the real code

---
+++ Real Code/Behaviors/Throwable.cs
@@ -0,0 +1,161 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RockCompany.Behaviors
+{
+    class Throwable : PhysicsProp
+    {
+        public Ray rockThrowRay;
+        public RaycastHit rockHit;
+        //public AnimationCurve rockFallCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.1f, 0.5f), new Keyframe(0.2f, 0.8f), new Keyframe(0.3f, 1f));
+        //public static AnimationCurve temp = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.1f, 0.3f), new Keyframe(0.3f, 1f));
+        public AnimationCurve rockFallCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.2f, 0.5f), new Keyframe(0.4f, 0.8f), new Keyframe(0.6f, 1f));
+        public static AnimationCurve temp = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.2f, 0.3f), new Keyframe(0.6f, 1f));
+        public AnimationCurve rockVerticalFallCurveNoBounce = temp;
+        public AnimationCurve rockVerticalFallCurve = temp;
+
+        private RaycastHit[] objectsHitByRock;
+        private List<RaycastHit> objectsHitByRockList = new List<RaycastHit>();
+
+        private int shovelMask = 11012424;
+
+        // REMOVE WHEN DONE DEBUGGING
+        internal ManualLogSource mls;
+        private const string modGUID = "Jaydesonia.RockCompany";
+
+
+
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            base.ItemActivate(used, buttonDown);
+            if (base.IsOwner)
+            {
+                objectsHitByRock = Physics.SphereCastAll(new Ray(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward),3f,20f, shovelMask, QueryTriggerInteraction.Collide);
+                objectsHitByRockList = objectsHitByRock.OrderBy((RaycastHit x) => x.distance).ToList();
+                PlayerControllerB localPlayerController = GameNetworkManager.Instance.localPlayerController;
+                for (int i = 0; i < objectsHitByRockList.Count; i++)
+                {
+                    if (objectsHitByRockList[i].GetType().Equals(typeof(Turret)))
+                    {
+                        
+                        break;
+                    }
+                    IHittable component;
+                    PlayerControllerB componentPlayer;
+                    RaycastHit hitInfo;
+                    if (objectsHitByRockList[i].transform.gameObject.layer == 8 || objectsHitByRockList[i].transform.gameObject.layer == 11)
+                    {
+                        string text = objectsHitByRockList[i].collider.gameObject.tag;
+                        for (int j = 0; j < StartOfRound.Instance.footstepSurfaces.Length; j++)
+                        {
+                            if (StartOfRound.Instance.footstepSurfaces[j].surfaceTag == text)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else if (objectsHitByRockList[i].transform.TryGetComponent<IHittable>(out component) && !(objectsHitByRockList[i].transform == playerHeldBy.transform) && (objectsHitByRockList[i].point == Vector3.zero || !Physics.Linecast(playerHeldBy.gameplayCamera.transform.position, objectsHitByRockList[i].point, out hitInfo, StartOfRound.Instance.collidersAndRoomMaskAndDefault)))
+                    {
+                        Vector3 forward = playerHeldBy.gameplayCamera.transform.forward;
+                        Debug.DrawRay(objectsHitByRockList[i].point, Vector3.up * 0.25f, Color.green, 5f);
+                        mls.LogInfo("OBJECT HIT BY ROCK SPHERECAST: " + objectsHitByRockList[i]);
+                        try
+                        {
+                            // Set to 1 for test, 101 for real
+                            int randomNumber = UnityEngine.Random.Range(0, 51);
+                            if (randomNumber == 0)
+                            {
+                                component.Hit(10, forward, playerHeldBy, playHitSFX: true);
+                            }
+                            else if (objectsHitByRockList[i].transform.TryGetComponent<PlayerControllerB>(out componentPlayer))
+                            {
+                                if(localPlayerController == componentPlayer)
+                                {
+                                    mls.LogInfo("LOCALPLAYERCONTROLLER DETECTED: " + localPlayerController.ToString());
+                                    localPlayerController.DamagePlayer(1,true, true, CauseOfDeath.Blast);
+                                }
+                            }
+                        }
+                        catch (Exception arg)
+                        {
+                            Debug.Log($"Exception caught when hitting object with shovel from player #{playerHeldBy.playerClientId}: {arg}");
+                        }
+                    }
+                    else if(objectsHitByRockList[i].transform.TryGetComponent<IHittable>(out component))
+                    {
+                        Vector3 forward = playerHeldBy.gameplayCamera.transform.forward;
+                        try
+                        {
+                            if (objectsHitByRockList[i].GetType().Equals(typeof(Turret)))
+                            {
+                                component.Hit(1, forward, playerHeldBy, playHitSFX: true);
+                                continue;
+                            }
+                        }
+                        catch (Exception arg)
+                        {
+                            Debug.Log($"Exception caught when hitting object with shovel from player #{playerHeldBy.playerClientId}: {arg}");
+                        }
+                    }
+                }
+                playerHeldBy.DiscardHeldObject(placeObject: true, null, GetRockThrowDestination());
+            }
+        }
+
+        public Vector3 GetRockThrowDestination()
+        {
+            Vector3 position = base.transform.position;
+            Debug.DrawRay(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward, Color.yellow, 15f);
+            rockThrowRay = new Ray(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward);
+            // MIGHT NEED TO SET StartOfRound.Instance.collidersRoomMaskDefaultAndPlayers back to collidersAndRoomMaskAndDefault
+            Debug.Log(StartOfRound.Instance.collidersAndRoomMaskAndDefault);
+            position = ((!Physics.Raycast(rockThrowRay, out rockHit, 20f, StartOfRound.Instance.collidersAndRoomMaskAndDefault)) ? rockThrowRay.GetPoint(16f) : rockThrowRay.GetPoint(rockHit.distance - 0.05f));
+            Debug.DrawRay(position, Vector3.down, Color.blue, 15f);
+            rockThrowRay = new Ray(position, Vector3.down);
+            if (Physics.Raycast(rockThrowRay, out rockHit, 30f, StartOfRound.Instance.collidersAndRoomMaskAndDefault))
+            {
+                return rockHit.point + Vector3.up * 0.05f;
+            }
+            return rockThrowRay.GetPoint(30f);
+        }
+
+        public override void EquipItem()
+        {
+            SetControlTipForRock();
+            EnableItemMeshes(enable: true);
+            isPocketed = false;
+        }
+
+        private void SetControlTipForRock()
+        {
+            string[] allLines = (new string[1] { "Throw Rock: [LMB]" });
+            if (base.IsOwner)
+            {
+                HUDManager.Instance.ChangeControlTipMultiple(allLines, holdingItem: true, itemProperties);
+            }
+        }
+
+        public override void FallWithCurve()
+        {
+            float magnitude = (startFallingPosition - targetFloorPosition).magnitude;
+            base.transform.rotation = Quaternion.Lerp(base.transform.rotation, Quaternion.Euler(itemProperties.restingRotation.x, base.transform.eulerAngles.y, itemProperties.restingRotation.z), 14f * Time.deltaTime / magnitude);
+            base.transform.localPosition = Vector3.Lerp(startFallingPosition, targetFloorPosition, rockFallCurve.Evaluate(fallTime));
+            if (magnitude > 5f)
+            {
+                base.transform.localPosition = Vector3.Lerp(new Vector3(base.transform.localPosition.x, startFallingPosition.y, base.transform.localPosition.z), new Vector3(base.transform.localPosition.x, targetFloorPosition.y, base.transform.localPosition.z), rockVerticalFallCurveNoBounce.Evaluate(fallTime));
+            }
+            else
+            {
+                base.transform.localPosition = Vector3.Lerp(new Vector3(base.transform.localPosition.x, startFallingPosition.y, base.transform.localPosition.z), new Vector3(base.transform.localPosition.x, targetFloorPosition.y, base.transform.localPosition.z), rockVerticalFallCurve.Evaluate(fallTime));
+            }
+            fallTime += Mathf.Abs(Time.deltaTime * 12f / magnitude);
+        }
+    }
+}
---
+++ Real Code/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using RockCompany.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+
+namespace RockCompany
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class RockCompanyBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Jaydesonia.RockCompany";
+        private const string modName = "Rock Company";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static RockCompanyBase Instance;
+
+        internal ManualLogSource mls;
+        public static AssetBundle MyCustomAssets;
+        private Throwable script;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The mod has awoke :)");
+
+            harmony.PatchAll(typeof(RockCompanyBase));
+
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            MyCustomAssets = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "mymodbundle/mymodassets"));
+            if (MyCustomAssets == null)
+            {
+                mls.LogError("Failed to load custom assets."); // ManualLogSource for your plugin
+                return;
+            }
+
+            //Spawn chance, set to 1000 for testing, 60 for not
+            int iRarity = 60;
+            Item customRock = MyCustomAssets.LoadAsset<Item>("Rock.asset");
+            script = customRock.spawnPrefab.AddComponent<Throwable>();
+            script.grabbable = true;
+            script.grabbableToEnemies = true;
+            script.isInFactory = true;
+            script.itemProperties = customRock;
+
+            customRock.minValue = 14;
+            customRock.maxValue = 28;
+            LethalLib.Modules.Utilities.FixMixerGroups(customRock.spawnPrefab);
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(customRock.spawnPrefab);
+            LethalLib.Modules.Items.RegisterScrap(customRock, iRarity, LethalLib.Modules.Levels.LevelTypes.All);
+            //LethalLib.Modules.Items.RegisterShopItem(customRock, 0);
+        }
+
+    }
+}
